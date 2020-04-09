c93c3af292ba38950e780c707c6a500478d9a78a
auctumnus <auctumnus@users.noreply.github.com>
2020-04-09 00:43:41 -0500

    fiat lux

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ const g = parentElement => {
   var cube = new THREE.Mesh(geometry, material)
   scene.add(cube)
 
+  var spotLight = new THREE.SpotLight( 0xffffff )
+  spotLight.position.set( 100, 1000, 100 )
+
+  spotLight.castShadow = true;
+
+  spotLight.shadow.mapSize.width = 1024
+  spotLight.shadow.mapSize.height = 1024
+
+  spotLight.shadow.camera.near = 500
+  spotLight.shadow.camera.far = 4000
+  spotLight.shadow.camera.fov = 30
+
+  scene.add( spotLight );
+
   camera.position.z = 5;
 
   let animate = () => {
