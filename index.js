7d52fee6b4bf8e3a297a34ba52a88dd23b14d132
auctumnus <auctumnus@users.noreply.github.com>
2020-04-09 00:22:57 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('loaded external script!')
 const g = parentElement => {
-  let width = parentElement.width
-  let height = parentElement.height
+  let width = parentElement.clientWidth
+  let height = parentElement.clientHeight
 
   let scene = new THREE.Scene()
   let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
