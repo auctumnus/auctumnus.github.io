9bfa7f3a9c9832b2ae06f9748c3b9d6c8b3bed27
auctumnus <auctumnus@users.noreply.github.com>
2020-04-09 00:18:37 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 console.log('loaded external script!')
-var main = function main() {
-  return 'test';
-};
+const g = parentElement => {
+  let width = parentElement.width
+  let height = parentElement.height
+
+  let scene = new THREE.Scene()
+  let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+
+  let renderer = new THREE.WebGLRenderer()
+  renderer.setSize(width, height)
+  parentElement.appendChild(renderer.domElement)
+
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+  }
+  animate();
+}
