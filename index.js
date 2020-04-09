c6acde10bdd2ea90c39b25da1430a48a821bf813
auctumnus <auctumnus@users.noreply.github.com>
2020-04-09 00:39:04 -0500

    oops

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const g = parentElement => {
   let height = parentElement.clientHeight
 
   let scene = new THREE.Scene()
-  scene.background = THREE.Color(window.getComputedStyle(parentElement).getPropertyValue('background-color'))
+  scene.background = THREE.Color('#000000')
   let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
 
   let renderer = new THREE.WebGLRenderer()
